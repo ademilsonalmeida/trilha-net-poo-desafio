05445400dadf1eed57db6ce5f492f36608309840
Ademilson Almeida <ademilsonalmeida@Ademilsons-MacBook-Pro.local>
2022-09-28 00:19:11 -0300

    Desafio finalizado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "12345", modelo: "Modelo 1", iMEI: "1111111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone");
+Smartphone iphone = new Iphone(numero: "67890", modelo: "Modelo 14", iMEI: "22222222", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
